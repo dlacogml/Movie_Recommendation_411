850e5920c8f0c5c6bbc91a5cecc6b0144d864069
Chaehee Lim <chaehee2@illinois.edu>
2021-04-25 15:15:19 -0500

    user id saved in session

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -1,7 +1,24 @@
 /*login.jsx*/
-import React from "react";
-/* We simply can use an array and loop and print each user */
+// import React from "react";
+import React, {Component, useState, useEffect} from "react";
+import Axios from 'axios';
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
+
+  const [userID, setUserID] = useState('');
+  
+  const saveUserID = () => {
+    Axios.post('http://localhost:3002/api/saveUserID', {
+      userID : userID
+    }).then(() => {
+      alert('success insert')
+    });
+  };
+ 
+  sessionStorage.setItem('userID', userID);
+  // const USER_ID = sessionStorage.getItem('userID');
+  
   return (
     <div>
         {/* <link href="signin.css" rel="stylesheet" /> */}
@@ -9,7 +26,9 @@ const LoginPage = () => {
           <form>
             <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
             <div className="form-floating">
-              <input type="email" className="form-control" id="floatingInput" placeholder="user id"/>
+              <input type="username" className="form-control" id="floatingInput" placeholder="user id" onChange={(e) => {
+        setUserID(e.target.value)
+        }}/>
               <label htmlFor="floatingInput">ID</label>
             </div>
             <div className="form-floating">
@@ -21,7 +40,7 @@ const LoginPage = () => {
                 <input type="checkbox" defaultValue="remember-me" /> Remember me
               </label>
             </div>
-            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <button className="w-100 btn btn-lg btn-primary" onClick = {saveUserID}><Link to="/AccountInfo">Sign in</Link></button>
           </form>
         </main>
       </div>
